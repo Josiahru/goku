e8c509341f074cce941f7c1aa527010616414494
Josiahru <100483584+Josiahru@users.noreply.github.com>
2023-04-15 17:31:03 +0000

    april 15 updates

--- expressExample/routes/empadd.js
+++ expressExample/routes/empadd.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,6 +7,29 @@ router.get('/', function(req, res, next) {
   res.render('empadd', { title: 'Products Are Us Employee Add' });
 });
 
-
+router.post('/', function(req, res, next) {
+  req.body.createdBy="bdpa_adminscott"; //Put in your own
+  req.body.modifiedBy="bdpa_adminscott"; //Put in your own
+  const fetch = require('node-fetch'); //We may need to npm install this...
+  const varHttpRequest = 'https://bdpamkedev.com/api/v5/employees';
+  fetch(varHttpRequest, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + global.DB_token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(req.body)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Message & Data ", data);
+    res.render('empadd', { title: 'Employee Add Successful', message: data.message, data: data.data });
+  })
+  .catch(error => {
+    console.error(error);
+    res.render('empadd', { title: 'Employee Add Error', message: error.message, data: error.data });
+    return "error";
+  })
+  });
 
 module.exports = router;
